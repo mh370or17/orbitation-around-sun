17d13d2685c01fe0d8e30288705f9f24e97a270f
mh370or17 <e.jazz.mobilink@gmail.com>
2024-11-14 18:54:53 +0000

    Add files via upload

---
+++ rotation.php
@@ -0,0 +1,251 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+<script>
+function starting_point_ho_chi_minh(){
+	$("#mh370-17-earth").animate({ left: '60px', top: '10px'}, 1010);
+	$("#mh370-17-earth_one").animate({ left: '138px', top: '95px'}, 1010);
+	$("#mh370-17-earth_two").animate({ left: '262px', top: '10px'}, 1010);
+	$("#mh370-17-earth_three").animate({ left: '340px', top: '95px'}, 1010);
+	$("#mh370-17-earth_four").animate({ left: '464px', top: '10px'}, 1010);
+	$("#mh370-17-earth_five").animate({ left: '542px', top: '94px'}, 1010);
+	$("#mh370-17-earth_six").animate({ left: '666px', top: '10px'}, 1010);
+	$("#mh370-17-earth_seven").animate({ left: '744px', top: '93px'}, 1010);
+	$("#mh370-17-earth_eight").animate({ left: '868px', top: '10px'}, 1010);
+	$("#mh370-17-earth_nine").animate({ left: '946px', top: '93px'}, 1010);
+	redirect_point_return_zero();
+}
+function redirect_point_return_zero(){
+	$("#mh370-17-earth").animate({ left: '37px', top: '95px'}, 1010);
+	$("#mh370-17-earth_one").animate({ left: '161px', top: '10px'}, 1010);
+	$("#mh370-17-earth_two").animate({ left: '239px', top: '95px'}, 1010);
+	$("#mh370-17-earth_three").animate({ left: '363px', top: '10px'}, 1010);
+	$("#mh370-17-earth_four").animate({ left: '441px', top: '94px'}, 1010);
+	$("#mh370-17-earth_five").animate({ left: '565px', top: '10px'}, 1010);
+	$("#mh370-17-earth_six").animate({ left: '643px', top: '94px'}, 1010);
+	$("#mh370-17-earth_seven").animate({ left: '767px', top: '10px'}, 1010);
+	$("#mh370-17-earth_eight").animate({ left: '845px', top: '93px'}, 1010);
+	$("#mh370-17-earth_nine").animate({ left: '969px', top: '10px'}, 1010);
+	starting_point_ho_chi_minh();
+}
+function redirect_point_return_one(){
+	starting_point_ho_chi_minh();
+}
+$(document).ready(function(){
+	starting_point_ho_chi_minh();
+});
+</script> 
+<style>
+	.globe {
+    	width:101px;
+        height: 100px;
+        border-radius:50%;
+        border: 1px solid #BEB5B0;
+		background-color:#E0C2BA;
+		float:left;
+    }
+	.globe_one {
+    	width:101px;
+        height: 100px;
+        border-radius:50%;
+        border: 1px solid #BEB5B0;
+		background-color:#E0C2BA;
+		float:left;
+    }
+	.globe {
+    	width:101px;
+        height: 100px;
+        border-radius:50%;
+        border: 1px solid #BEB5B0;
+		background-color:#E0C2BA;
+		float:left;
+    }
+	.globe_two {
+    	width:101px;
+        height: 100px;
+        border-radius:50%;
+        border: 1px solid #BEB5B0;
+		background-color:#E0C2BA;
+		float:left;
+    }
+	.globe_three {
+    	width:101px;
+        height: 100px;
+        border-radius:50%;
+        border: 1px solid #BEB5B0;
+		background-color:#E0C2BA;
+		float:left;
+    }
+	.globe_four {
+    	width:101px;
+        height: 100px;
+        border-radius:50%;
+        border: 1px solid #BEB5B0;
+		background-color:#E0C2BA;
+		float:left;
+    }
+	.globe_five {
+    	width:101px;
+        height: 100px;
+        border-radius:50%;
+        border: 1px solid #BEB5B0;
+		background-color:#E0C2BA;
+		float:left;
+    }
+	.globe_six {
+    	width:101px;
+        height: 100px;
+        border-radius:50%;
+        border: 1px solid #BEB5B0;
+		background-color:#E0C2BA;
+		float:left;
+    }
+	.globe_seven {
+    	width:101px;
+        height: 100px;
+        border-radius:50%;
+        border: 1px solid #BEB5B0;
+		background-color:#E0C2BA;
+		float:left;
+    }
+	.globe_eight {
+    	width:101px;
+        height: 100px;
+        border-radius:50%;
+        border: 1px solid #BEB5B0;
+		background-color:#E0C2BA;
+		float:left;
+    }
+	.globe_nine {
+    	width:101px;
+        height: 100px;
+        border-radius:50%;
+        border: 1px solid #BEB5B0;
+		background-color:#E0C2BA;
+		float:left;
+    }
+    .sphere {
+    	background: #806043;
+    	height: 9px;
+    	width: 10px;
+    	position: absolute;
+    	left: 37px;
+    	top: 95px;
+    	border-radius: 50%;
+       }
+	  .sphere_one {
+    	background: #806043;
+    	height: 9px;
+    	width: 10px;
+    	position: absolute;
+    	left: 161px;
+    	top: 10px;
+    	border-radius: 50%;
+       }
+	   .sphere_two {
+    	background: #806043;
+    	height: 9px;
+    	width: 10px;
+    	position: absolute;
+    	left: 262px;
+    	top: 10px;
+    	border-radius: 50%;
+       }
+	   .sphere_three {
+    	background: #806043;
+    	height: 9px;
+    	width: 10px;
+    	position: absolute;
+    	left: 340px;
+    	top: 95px;
+    	border-radius: 50%;
+       }
+	   .sphere_four {
+    	background: #806043;
+    	height: 9px;
+    	width: 10px;
+    	position: absolute;
+    	left: 464px;
+    	top: 10px;
+    	border-radius: 50%;
+       }
+	   .sphere_five {
+    	background: #806043;
+    	height: 9px;
+    	width: 10px;
+    	position: absolute;
+    	left: 542px;
+    	top: 94px;
+    	border-radius: 50%;
+       }
+	   .sphere_six {
+    	background: #806043;
+    	height: 9px;
+    	width: 10px;
+    	position: absolute;
+    	left: 666px;
+    	top: 10px;
+    	border-radius: 50%;
+       }
+	   .sphere_seven {
+    	background: #806043;
+    	height: 9px;
+    	width: 10px;
+    	position: absolute;
+    	left: 744px;
+    	top: 93px;
+    	border-radius: 50%;
+       }
+	   .sphere_eight {
+    	background: #806043;
+    	height: 9px;
+    	width: 10px;
+    	position: absolute;
+    	left: 868px;
+    	top: 10px;
+    	border-radius: 50%;
+       }
+	   .sphere_nine {
+    	background: #806043;
+    	height: 9px;
+    	width: 10px;
+    	position: absolute;
+    	left: 946px;
+    	top: 93px;
+    	border-radius: 50%;
+       }
+</style>
+</head>
+<body>
+<div class='globe' id="earth-sun"></div>
+<div class="sphere" id="mh370-17-earth"></div>
+
+<div class='globe_one' id="earth-sun_one"></div>
+<div class="sphere_one" id="mh370-17-earth_one"></div>
+
+<div class='globe_two' id="earth-sun_two"></div>
+<div class="sphere_two" id="mh370-17-earth_two"></div>
+
+<div class='globe_three' id="earth-sun_three"></div>
+<div class="sphere_three" id="mh370-17-earth_three"></div>
+
+<div class='globe_four' id="earth-sun_four"></div>
+<div class="sphere_four" id="mh370-17-earth_four"></div>
+
+<div class='globe_five' id="earth-sun_five"></div>
+<div class="sphere_five" id="mh370-17-earth_five"></div>
+
+<div class='globe_six' id="earth-sun_six"></div>
+<div class="sphere_six" id="mh370-17-earth_six"></div>
+
+<div class='globe_seven' id="earth-sun_seven"></div>
+<div class="sphere_seven" id="mh370-17-earth_seven"></div>
+
+<div class='globe_eight' id="earth-sun_eight"></div>
+<div class="sphere_eight" id="mh370-17-earth_eight"></div>
+
+<div class='globe_nine' id="earth-sun_nine"></div>
+<div class="sphere_nine" id="mh370-17-earth_nine"></div>
+</body>
+</html>
